218991c711de4da969884fec7af63f9fbf135042
Raul Gallegos <raul.ogh@gmail.com>
2013-05-05 23:00:49 -0500

    CGAL script added

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+#include <CGAL/point_generators_2.h>
+#include <CGAL/circulator.h>
+#include <CGAL/Triangulation_face_base_with_info_2.h>
+
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+
+int main(){
+
+	return 0;
+}
